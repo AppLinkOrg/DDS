2652cdd2130dfe7fb72727cddae7d22fd4252d3f
992613643 <992613643@qq.com>
2018-11-16 11:13:52 +0800

    qqqq

---
+++ source/pages/driver/driver.js
@@ -0,0 +1,87 @@
+// pages/driver/driver.js
+import { AppBase } from "../../appbase";
+import { ApiConfig } from "../../apis/apiconfig";
+import { InstApi } from "../../apis/inst.api.js";
+import { OrderApi } from "../../apis/order.api.js";
+class Content extends AppBase {
+  constructor() {
+    super();
+  }
+  onLoad(options) {
+    this.Base.Page = this;
+    //options.id=5;
+    
+
+    this.setData({
+      array: ['所有', '离我最近', '费用最高' ],
+      objectArray: [
+        {
+          id: 0,
+          name: '所有'
+        },
+        {
+          id: 1,
+          name: '离我最近'
+        },
+        {
+          id: 2,
+          name: '费用最高'
+        }
+     
+      ],
+      index:0,
+      tab:0,
+     
+      
+    })
+   
+    super.onLoad(options);
+    
+  }
+  
+  
+  //界面标题
+  setPageTitle() {
+    wx.setNavigationBarTitle({
+      title: '司机首页',
+      
+    });
+  }
+
+  
+  onMyShow() {
+    
+    var that = this;
+    
+    var orderapi=new OrderApi();
+    orderapi.list({}, (list) => {
+      this.Base.setMyData({ list });
+      var num=0;
+             for(var i=1;i<list.length;i++){
+               if (list[0].release_status_name="报名中")
+               {
+                 num++;
+               }       
+             }
+
+      this.Base.setMyData({ num: num });
+    });
+  }
+  qwe(e) {
+    this.setData({
+      index: e.detail.value
+    })
+  }
+  changetab(e) {
+    this.Base.setMyData({ tab: e.currentTarget.id });
+  }
+  
+}
+var tab =null;
+var content = new Content();
+var body = content.generateBodyJson();
+body.onLoad = content.onLoad;
+body.onMyShow = content.onMyShow;
+body.qwe=content.qwe;
+body.changetab = content.changetab;
+Page(body)
---
+++ source/pages/rz/rz.js
@@ -0,0 +1,31 @@
+// pages/driver/driver.js
+import { AppBase } from "../../appbase";
+import { ApiConfig } from "../../apis/apiconfig";
+import { InstApi } from "../../apis/inst.api.js";
+
+class Content extends AppBase {
+  constructor() {
+    super();
+  }
+  onLoad(options) {
+    this.Base.Page = this;
+    //options.id=5;
+    super.onLoad(options);
+
+  }
+  //界面标题
+  setPageTitle() {
+    wx.setNavigationBarTitle({
+      title: '司机认证',
+
+    });
+  }
+  onMyShow() {
+    var that = this;
+  }
+}
+var content = new Content();
+var body = content.generateBodyJson();
+body.onLoad = content.onLoad;
+body.onMyShow = content.onMyShow;
+Page(body)
